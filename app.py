90030a8c5e28fd617e84acf7460cf26e16d67eae
DennyAlexanderCM <alexanderchali195@gmail.com>
2022-02-16 13:23:12 -0600

    Añadido el primer análizador

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from tkinter import *
+from tkinter import Tk
 from tkinter import filedialog
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 def pedirNumeroEntero():
     correcto=False
@@ -12,10 +15,96 @@ def pedirNumeroEntero():
             print('Error, introduce un numero entero')
     return num 
 
-def leerArchivo():
-    root = filedialog.askopenfilename(title= "Abrir Archivo", filetypes=(("Data","*.data"),("Todos los archivos","*.*")))
+def leerArchivo(tipo):
+    root = filedialog.askopenfilename(title= "Abrir Archivo", filetypes=(('File',f'*{tipo}'),('All Files','*.*')))
     if root != "":
-        file = open(root,'r')
-        contentFile = file.read()
+        #encoding utf-8
+        file = open(root,'r',encoding='utf-8')
+        #En dado caso hayan espacios al incio y al final de la cadena strip se encarga de eliminarlos
+        contentFile = file.read().strip()
+        #Convertimos todo el documento en minusculas
+        contentFile = cleanData(contentFile.lower())
         return contentFile
     return None
+
+def cleanData(data):
+    txt = ""
+    add = False
+    for caracter in data:
+        if caracter != '\"':
+            if (caracter != " " and caracter != "\t" and caracter != "\n") or add:
+                txt += caracter
+        elif not add:
+            txt += caracter
+            add = True
+        else:
+            txt += caracter
+            add = False    
+    return txt
+
+def analizarDatos(data):
+    #Mes
+    estadoMes = False
+    nombreMes = ""
+    #año
+    estadoAnio = False
+    anio = ""
+    #productos
+    addProducts = True
+    productsList = []
+
+    #producto
+    addProductA = False
+    addProductB = False
+
+    #comillas 
+    state = 0
+  
+    aux = ""
+
+    for caracter in data:
+        if estadoMes == False:
+            if caracter != ":":
+                aux += caracter
+            else:
+                nombreMes = aux
+                estadoMes = True
+                aux = ""
+        elif estadoAnio == False:
+            if caracter != "=":
+                aux += caracter
+            else:
+                anio = aux
+                estadoAnio = True
+                aux = ""
+        elif caracter == "(":
+            addProducts = True
+        
+        elif caracter == ")":
+            print("Archivo leído correctamente")
+
+        elif addProducts == True and estadoAnio == True and estadoMes == True:
+            if caracter == "[":
+                addProductA = True
+            elif caracter == "]":
+                addProductB = True
+
+            elif caracter == '\"':
+                state += 1
+            elif caracter == ";" and addProductA == True and addProductB == True and state ==2:
+                lista = aux.split(",")
+                lista[1] = float(lista[1])
+                lista[2] = int(lista[2])
+                productsList.append(lista)
+                addProductA = False
+                addProductB = False
+                state = 0
+                aux = ""
+            else:
+                aux += caracter
+
+    print(nombreMes, anio)
+    print(productsList)
+
+def analizarInstrucciones(data):
+    pass
--- main.py
+++ main.py
@@ -1,35 +1,42 @@
 from app import *
 
 def run():
-    txtData = None
-    txtInstrucciones = None
+    txtData = ""
+    txtInstrucciones = ""
     end = False
     selection = 0
+    
     while not end:
+
         print("------------Menú------------\n1. Cargar Data\n2. Cargar Instrucciones\n3. Análizar\n4. Reportes\n5. Salir")
         selection = pedirNumeroEntero()
 
         if selection == 1:
 
-            txtData = leerArchivo()
-            if rute != None:
-                print(txtData)
+            txtData = leerArchivo(".data")
+            if txtData != None:
+                print("Datos cargados...\n")
             else:
                 print("sin Cambios")
 
         elif selection == 2:
-            txtInstrucciones = leerArchivo()
+            txtInstrucciones = leerArchivo(".lfp")
             if txtInstrucciones != None:
+                print("Datos cargados...\n")
                 print(txtInstrucciones)
             else:
                 print("sin Cambios")
+
         elif selection == 3:
-            dataAnalizado = analizarData(txtData)
+            analizarDatos(txtData)
+
         elif selection == 4:
-            print("3")
+            crearGraficaSectores()
+
         elif selection == 5:
             print("Finalizando programa...")
             end = True
+
         else:
             print("Intente de nuevo")
 
